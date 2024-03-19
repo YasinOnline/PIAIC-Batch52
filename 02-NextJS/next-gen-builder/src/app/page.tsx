fcdf9d5ea04a95824a42a40efdb7ee58d95d640e
Mohammad Yasin <yasin.online@outlook.com>
2024-03-20 00:05:43 +0500

    First Customized NextJS Project Created.

---
+++ 02-NextJS/next-gen-builder/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Assalam-o-Alaikum,</h1>
+      <h1>I am <strong>Mohammad Yasin</strong> and this is my first Next.js project.</h1>
+    </div>
+  );
+}
